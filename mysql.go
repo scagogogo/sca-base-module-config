6f874bee07437a2c5b70272ba6cfa528c53a1ef7
cc11001100 <cc11001100@qq.com>
2023-07-12 18:10:51 +0800

    feat: 增加mysql选项

--- mysql.go
+++ mysql.go
@@ -2,6 +2,16 @@ package sca_base_module_config
 
 type Mysql struct {
 
+	// 自动初始化MySQL驱动
+	AutoInit *bool `yaml:"auto-init" mapstructure:"auto-init"`
+
 	// Example: "xxxx:xxxxxxxx@tcp(xxxx)/xxxx?parseTime=true&loc=Local&charset=utf8mb4"
 	DSN string `yaml:"dsn" mapstructure:"dsn"`
+
+	Driver MysqlDriverEnable `yaml:"driver" mapstructure:"driver"`
+}
+
+type MysqlDriverEnable struct {
+	SqlX *bool `yaml:"sqlx" mapstructure:"sqlx"`
+	Gorm *bool `yaml:"gorm" mapstructure:"gorm"`
 }
