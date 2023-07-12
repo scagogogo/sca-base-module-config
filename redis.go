02d4103517c961e9a4095fe5823d2a6eca94ba30
cc11001100 <cc11001100@qq.com>
2023-07-12 19:02:32 +0800

    feat: redis增加auto-init

--- redis.go
+++ redis.go
@@ -1,8 +1,9 @@
 package sca_base_module_config
 
 type Redis struct {
-	Address string `yaml:"address" mapstructure:"address"`
-	Passwd  string `yaml:"passwd" mapstructure:"passwd"`
+	AutoInit *bool  `yaml:"auto-init"  mapstructure:"auto-init"`
+	Address  string `yaml:"address" mapstructure:"address"`
+	Passwd   string `yaml:"passwd" mapstructure:"passwd"`
 }
 
 func (x Redis) IsOk() bool {
