b65b8af6ef6c0f885e0871bfad317d3934b9be81
cc11001100 <cc11001100@qq.com>
2023-07-13 10:24:17 +0800

    feat: logger增加级别

--- logger.go
+++ logger.go
@@ -2,6 +2,7 @@ package sca_base_module_config
 
 type Logger struct {
 	AutoInit  *bool        `yaml:"auto-init" mapstructure:"auto-init"`
+	Level     string       `yaml:"level" mapstructure:"level"`
 	Enable    LoggerEnable `yaml:"enable" mapstructure:"enable"`
 	Directory string       `yaml:"directory" mapstructure:"directory"`
 }
